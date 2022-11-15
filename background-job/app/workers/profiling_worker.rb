5f0a7c7ae406d8a11cfa3044a1375eedc25b23f8
Edivan de Castro <edivandecastro@gmail.com>
2022-11-15 11:36:47 -0300

    Implementing fault queue with maxretry handler

--- background-job/app/workers/profiling_worker.rb
+++ background-job/app/workers/profiling_worker.rb
@@ -1,17 +1,20 @@
+require 'sneakers/handlers/maxretry'
+
 class ProfilingWorker
   include Sneakers::Worker
 
   from_queue 'downloads',
              exchange: 'download_process',
-             timeout_job_after: 1
+             handler: Sneakers::Handlers::Maxretry,
+             retry_max_times: 5,
+             retry_timeout: 20.seconds.in_milliseconds,
+             arguments: { 'x-dead-letter-exchange': 'downloads-retry' }
 
   def work(message)
-    begin
-      logger.info JSON.parse(message)
-    rescue StandardError => e
-      logger.error e
-    end
-
+    logger.info JSON.parse(message)
     ack!
+  rescue StandardError => e
+    logger.error e
+    reject!
   end
 end
